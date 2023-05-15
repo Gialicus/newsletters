572f0cc81f27ea7971aa6f061ea791707019f846
g.coletta <giali.coletta@fullstackagency.it>
2023-05-15 23:40:16 +0200

    dockerfile

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,21 @@ use newsletters::{
     telemetry::{get_subscriber, init_subscriber},
 };
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("newsletters".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Fail to read configuration");
-    let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
-            .expect("Fail to connect to database");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Fail to connect to database");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listner = TcpListener::bind(address)?;
     startup::run(listner, connection_pool)?.await
 }
